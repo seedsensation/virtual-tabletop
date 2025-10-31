f1ea3d9937609f6401dcb7dff150b7efc3333355
N1311218 <christian.etchells2024@my.ntu.ac.uk>
2025-10-31 21:15:41 +0000

    select sheet progress
    this now adds temp to the option box

--- scripts/selectCharSheet.cs
+++ scripts/selectCharSheet.cs
@@ -2,20 +2,21 @@ using Godot;
 using System;
 using System.IO;
 using System.Runtime.ExceptionServices;
+using System.Threading.Tasks.Dataflow;
 
-public partial class SheatSelect : VBoxContainer
+public partial class selectCharSheet : OptionButton
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		//select the sheats from the files
+		chooseSheats();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//select the sheats from the files
-		chooseSheats();
+		
 	}
 
 	public void chooseSheats()
@@ -23,7 +24,7 @@ public partial class SheatSelect : VBoxContainer
 		string[] fileNames = Directory.GetFiles("data");
 		for (int i = 0; i < fileNames.Length; i++)
 		{
-			
+			this.AddItem("temp");
 		}
 	}
 }
