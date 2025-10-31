d8419a27329862bc85864ca6375e543895b311c0
Mercury <seedsensation@icloud.com>
2025-10-31 19:18:07 +0000

    Added panels for sheet creation

--- scripts/SheetCreatorPanel.cs
+++ scripts/SheetCreatorPanel.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using VirtualTabletop.Types;
+namespace VirtualTabletop.Sheets;
 
 public partial class SheetCreatorPanel : Panel
 {
@@ -11,5 +13,18 @@ public partial class SheetCreatorPanel : Panel
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void ItemSelected(int index) {
+		SheetType type = (SheetType)index;
+		GD.Print(index);
+		Label title = (Label)GetNode("Demonstration");
+		title.Text = type switch {
+			SheetType.TypeInt => "10",
+			SheetType.TypeFloat => "1.5",
+			SheetType.TypeString => "Hello World",
+			SheetType.TypeBool => "[  ]",
+			SheetType.TypeDepletable => "10 / 15"
+			};
 	}
 }
