cc04c5f60169c9b427ba695b0dde98d1061d1529
N1311218 <christian.etchells2024@my.ntu.ac.uk>
2025-10-31 23:39:11 +0000

    changes depending on sheet selection

--- scripts/selectCharSheet.cs
+++ scripts/selectCharSheet.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks.Dataflow;
 public partial class selectCharSheet : OptionButton
 {
 	// Called when the node enters the scene tree for the first time.
+	[Export]
+	public string chosenPlayerSheet, chosenEnemySheet;
 	public override void _Ready()
 	{
 		//select the sheats from the files
 		chooseSheats();
+		readChoice(0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,7 +27,21 @@ public partial class selectCharSheet : OptionButton
 		string[] fileNames = Directory.GetFiles("data");
 		for (int i = 0; i < fileNames.Length; i++)
 		{
-			this.AddItem(fileNames[i].Remove(0,5));
+			this.AddItem(fileNames[i].Remove(0, 5));
+		}
+	}
+	
+	public void readChoice(int index)
+	{
+		if (this.Name == "SelectSheetPlayer")
+		{
+			chosenPlayerSheet = GetItemText(index);
+			GD.Print(chosenPlayerSheet);
+		}
+		else
+		{
+			chosenEnemySheet = GetItemText(index);
+			GD.Print(chosenEnemySheet);
 		}
 	}
 }
