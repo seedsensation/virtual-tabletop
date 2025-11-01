18fdeefb6eab601aec53c5c036f5f53cab5cdc2b
RainSparks29 <hsouthall29@gmail.com>
2025-11-01 05:00:54 +0000

    Added menu for selecting roll type

---
+++ FaLineEdit.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class FaLineEdit : LineEdit
+{
+	public void validateInput(string newText)
+	{
+		int diceValue;
+		try
+		{
+			diceValue = Convert.ToInt32(newText);
+		}
+		catch (System.Exception)
+		{
+			if (newText != "-")
+			{
+				this.Clear();
+			}
+		}
+	}
+}
