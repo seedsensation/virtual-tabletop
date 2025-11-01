a39de52ff088738ce53f68939721b99dcef91eed
Mercury <seedsensation@icloud.com>
2025-11-01 07:51:41 +0000

    Completed Sheet Editor

--- scripts/selectCharSheet.cs
+++ scripts/selectCharSheet.cs
@@ -26,13 +26,15 @@ public partial class selectCharSheet : OptionButton
 	public void chooseSheats()
 	{
 		string[] fileNames = Directory.GetFiles(ProjectSettings.GlobalizePath("user://"));
+		GD.Print(GetParent().GetNode<TextEdit>("SystemName").Text);
 		var fileNamesCollection =
 			from file in fileNames
-			where file.StartsWith(GetParent().GetNode<TextEdit>("SystemName").Text.ToLower())
+			where file.GetFile().ToLower().StartsWith(GetParent().GetNode<TextEdit>("SystemName").Text.ToLower())
 			where file.GetExtension() == "json"
 			select file.GetFile();
 
 		foreach ( string i in fileNamesCollection) {
+			GD.Print(i);
 			this.AddItem(i);
 		}
 	}
